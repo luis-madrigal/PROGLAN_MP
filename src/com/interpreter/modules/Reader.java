f81d052f582aa81efdc8d8c5bff68c71a0e0e9a4
kierstengyra <ramoskierstengyra@gmail.com>
2017-11-15 22:58:51 +0800

    Added Writer and Reader modules
    Not yet tested

---
+++ src/com/interpreter/modules/Reader.java
@@ -0,0 +1,42 @@
+package com.interpreter.modules;
+
+import javax.swing.JOptionPane;
+
+public class Reader {
+	
+	public static int readIntegerInput(String msg) {
+		String input = JOptionPane.showInputDialog(msg);
+		return Integer.parseInt(input);
+	}
+	
+	public static float readFloatInput(String msg) {
+		String input = JOptionPane.showInputDialog(msg);
+		return Float.parseFloat(input);
+	}
+	
+	public static double readDoubleInput(String msg) {
+		String input = JOptionPane.showInputDialog(msg);
+		return Double.parseDouble(input);
+	}
+	
+	public static long readLongInput(String msg) {
+		String input = JOptionPane.showInputDialog(msg);
+		return Long.parseLong(input);
+	}
+	
+	public static short readShortInput(String msg) {
+		String input = JOptionPane.showInputDialog(msg);
+		return Short.parseShort(input);
+	}
+	
+	public static char readCharInput(String msg) {
+		String input = JOptionPane.showInputDialog(msg);
+		return input.charAt(0);
+	}
+	
+	public static boolean readBoolInput(String msg) {
+		String input = JOptionPane.showInputDialog(msg);
+		return Boolean.parseBoolean(input);
+	}
+	
+}
---
+++ src/com/interpreter/modules/Writer.java
@@ -0,0 +1,39 @@
+package com.interpreter.modules;
+
+import com.utils.Console;
+
+public class Writer {
+	
+	public static void printText(int output) {
+		Console.instance().log(String.valueOf(output));
+	}
+	
+	public static void printText(float output) {
+		Console.instance().log(String.valueOf(output));
+	}
+	
+	public static void printText(double output) {
+		Console.instance().log(String.valueOf(output));
+	}
+	
+	public static void printText(short output) {
+		Console.instance().log(String.valueOf(output));
+	}
+	
+	public static void printText(long output) {
+		Console.instance().log(String.valueOf(output));
+	}
+	
+	public static void printText(boolean output) {
+		Console.instance().log(String.valueOf(output));
+	}
+	
+	public static void printText(char output) {
+		Console.instance().log(output+" ");
+	}
+	
+	public static void printText(String output) {
+		Console.instance().log(output);
+	}
+	
+}
