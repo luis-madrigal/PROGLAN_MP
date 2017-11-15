586ebb56763bb8d2f2d203d3fa6bfd2071eef301
Luis Madrigal <lmadrigal.111@gmail.com>
2017-11-15 22:11:14 +0800

    removed error in TerminalNode

--- src/com/interpreter/AST/TerminalNode.java
+++ src/com/interpreter/AST/TerminalNode.java
@@ -6,13 +6,17 @@ import com.utils.KeyTokens;
  * for terminals (Variables/Literals)
  */
 public class TerminalNode extends AbstractSyntaxTree {
-    protected KeyTokens.TERMINAL_TYPE type;
+	protected KeyTokens.TERMINAL_TYPE type;
     protected Object value;
 
-    public TerminalNode(KeyTokens.TERMINAL_TYPE type, Object value){
-        this.type = type;
-        this.value = value;
-    }
+    public TerminalNode(AbstractSyntaxTree parent) {
+		super(parent);
+		// TODO Auto-generated constructor stub
+	}
+//    public TerminalNode(KeyTokens.TERMINAL_TYPE type, Object value){
+//        this.type = type;
+//        this.value = value;
+//    }
 
     public KeyTokens.TERMINAL_TYPE getType() {
         return type;
