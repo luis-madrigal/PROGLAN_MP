1464dd223bb287a11be6021758287ee33d19884e
kierstengyra <ramoskierstengyra@gmail.com>
2017-11-16 12:34:29 +0800

    Edited Reader & Writer

--- src/com/interpreter/modules/Writer.java
+++ src/com/interpreter/modules/Writer.java
@@ -5,31 +5,31 @@ import com.utils.Console;
 public class Writer {
 	
 	public static void printText(int output) {
-		Console.instance().log(String.valueOf(output));
+		Console.instance().logln(String.valueOf(output));
 	}
 	
 	public static void printText(float output) {
-		Console.instance().log(String.valueOf(output));
+		Console.instance().logln(String.valueOf(output));
 	}
 	
 	public static void printText(double output) {
-		Console.instance().log(String.valueOf(output));
+		Console.instance().logln(String.valueOf(output));
 	}
 	
 	public static void printText(short output) {
-		Console.instance().log(String.valueOf(output));
+		Console.instance().logln(String.valueOf(output));
 	}
 	
 	public static void printText(long output) {
-		Console.instance().log(String.valueOf(output));
+		Console.instance().logln(String.valueOf(output));
 	}
 	
 	public static void printText(boolean output) {
-		Console.instance().log(String.valueOf(output));
+		Console.instance().logln(String.valueOf(output));
 	}
 	
 	public static void printText(char output) {
-		Console.instance().log(output+" ");
+		Console.instance().logln(output+" ");
 	}
 	
 	public static void printText(String output) {
