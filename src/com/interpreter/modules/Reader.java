45aa1ac36036892a0f7ef47016d1f54ee03d38f5
Neil Romblon <neilromblon@live.com>
2017-11-23 15:49:29 +0800

    updated reader

--- src/com/interpreter/modules/Reader.java
+++ src/com/interpreter/modules/Reader.java
@@ -3,45 +3,45 @@ package com.interpreter.modules;
 import javax.swing.JOptionPane;
 
 public class Reader {
-	
-	public static int readIntegerInput() {
-		String input = JOptionPane.showInputDialog("Enter integer value:");
-		return Integer.parseInt(input);
-	}
-	
-	public static float readFloatInput() {
-		String input = JOptionPane.showInputDialog("Enter float value:");
-		return Float.parseFloat(input);
-	}
-	
-	public static double readDoubleInput() {
-		String input = JOptionPane.showInputDialog("Enter double value:");
-		return Double.parseDouble(input);
-	}
-	
-	public static long readLongInput() {
-		String input = JOptionPane.showInputDialog("Enter long value:");
-		return Long.parseLong(input);
-	}
-	
-	public static short readShortInput() {
-		String input = JOptionPane.showInputDialog("Enter short value:");
-		return Short.parseShort(input);
-	}
-	
-	public static char readCharInput() {
-		String input = JOptionPane.showInputDialog("Enter char value:");
-		return input.charAt(0);
-	}
-	
-	public static boolean readBoolInput() {
-		String input = JOptionPane.showInputDialog("Enter true or false:");
-		return Boolean.parseBoolean(input);
-	}
-	
-	public static String readStringInput() {
-		String input = JOptionPane.showInputDialog("Enter string:");
+
+	public static String readInput(){
+		String input = JOptionPane.showInputDialog("Enter Input: ");
 		return input;
 	}
-	
+
+	public static int readIntegerInput(String msg) {
+		String input = JOptionPane.showInputDialog(msg);
+		return Integer.parseInt(input);
+	}
+
+	public static float readFloatInput(String msg) {
+		String input = JOptionPane.showInputDialog(msg);
+		return Float.parseFloat(input);
+	}
+
+	public static double readDoubleInput(String msg) {
+		String input = JOptionPane.showInputDialog(msg);
+		return Double.parseDouble(input);
+	}
+
+	public static long readLongInput(String msg) {
+		String input = JOptionPane.showInputDialog(msg);
+		return Long.parseLong(input);
+	}
+
+	public static short readShortInput(String msg) {
+		String input = JOptionPane.showInputDialog(msg);
+		return Short.parseShort(input);
+	}
+
+	public static char readCharInput(String msg) {
+		String input = JOptionPane.showInputDialog(msg);
+		return input.charAt(0);
+	}
+
+	public static boolean readBoolInput(String msg) {
+		String input = JOptionPane.showInputDialog(msg);
+		return Boolean.parseBoolean(input);
+	}
+
 }
