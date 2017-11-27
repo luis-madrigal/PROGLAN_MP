519e48f0d358c19f27c4b7ab37af500f9c840f37
Candy <candy.espulgar@gmail.com>
2017-11-28 03:07:05 +0900

    Breakpoint New Line Bug Fix

--- src/com/ide/Panel.java
+++ src/com/ide/Panel.java
@@ -1084,12 +1084,19 @@ public class Panel implements CaretListener, Runnable, ActionListener, KeyListen
 	public Stack<Integer> getListBreakpoints(String text) {
 		Stack<Integer> listBreakpoints = new Stack<Integer>();
 		String listText = codeInput.getText();
-		int length = listText.split("\n").length;
+		String[] strText= listText.split("\n");
+		int length = strText.length; //listText.split("\n").length;
 		
 		for(int i = length-1; i >= 0; i--) {
 			if(gutter.hasBookmark(i)) {
+				if(i-1 >= 0 &&  strText[i-1].trim().length() > 0) {
 
-				listBreakpoints.push(i);
+					listBreakpoints.push(i);
+				}
+				else {
+					
+					listBreakpoints.push(getPreviousLineWithText(i, strText));
+				}
 //				System.out.println(i+" has");
 			}
 		}
@@ -1105,6 +1112,31 @@ public class Panel implements CaretListener, Runnable, ActionListener, KeyListen
 //		listBreakpoints.push(7);
 		
 		return listBreakpoints;
+	}
+	
+	public int getPreviousLineWithText(int from, String[] listText) {
+		boolean notMultilineComment = true;
+		for(int i = from-1; i >= 0; i--) {
+			
+			// When a *[] is encountered, consider the text as a multiline comment
+			// until a []* is seen.
+			if(listText[i].contains("*[]")) {
+				notMultilineComment = false;
+			}
+			else if(listText[i].contains("[]*")) {
+				notMultilineComment = true;
+			}
+			
+			if(listText[i].trim().length() > 0 && 
+					!listText[i].contains("]:") &&
+					!listText[i].contains("[]*") &&
+					!listText[i].contains("*[]") &&
+					notMultilineComment) {
+				return i;
+			}
+		}
+		return 0;
+		
 	}
 	@Override
 	public void keyReleased(KeyEvent e) {
