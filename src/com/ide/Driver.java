dfd9ae33a50713561bc24250108860d4d8cf1a25
Candy <candy.espulgar@gmail.com>
2017-12-02 14:23:21 +0900

    Removed Driver Comments

--- src/com/ide/Driver.java
+++ src/com/ide/Driver.java
@@ -2,26 +2,6 @@ package com.ide;
 
 import com.threads.MainThread;
 
-/**
- * Uses RSyntaxTextArea
- * https://github.com/bobbylight/RSyntaxTextArea
- * 
- * Requires Gradle to build
- * Installation Guide: https://gradle.org/releases/
- * 
- * 
- * Requires jdk1.8.0_65
- * Check your <C:/Program Files/Java> or <C:/Program Files (x86)/Java> if you have it
- * Download: http://www.oracle.com/technetwork/pt/java/javase/downloads/jdk8-downloads-2133151.html
- * 
- * Then add the bin <C:\Program Files\Java\jdk1.8.0_65> to your environment variables under PATH
- * 
- * 
- * Import Gradle > RSyntaxTextArea-master
- * Configure Build Path > Projects
- * @author Candy
- *
- */
 public class Driver {
 	public static void main(String[] args) {
 		MainThread threadMain = new MainThread();
